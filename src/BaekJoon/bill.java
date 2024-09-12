c0306a07f1685f05a1df419522bd52d8b8b2a00a
Pjangwon <pjw051016@gmail.com>
2024-09-12 17:27:23 +0900

    bill 1차 기능 구현

--- src/BaekJoon/bill.java
+++ src/BaekJoon/bill.java
@@ -1,4 +1,23 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class bill {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int X = sc.nextInt();
+    int N = sc.nextInt();
+    int sum = 0;
+    String result;
+
+    for (int i = 1; i <= N; i++) {
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+
+      sum += a * b;
+    }
+    result = (sum == X) ? "Yes" : "No";
+    System.out.println(result);
+  }
 }
