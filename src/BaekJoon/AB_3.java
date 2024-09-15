27718f8c09f00609fcc33c5bf28e8d512b965963
Pjangwon <pjw051016@gmail.com>
2024-09-15 16:17:39 +0900

    AB_3 향상된 for문 대체

--- src/BaekJoon/AB_3.java
+++ src/BaekJoon/AB_3.java
@@ -7,12 +7,17 @@ public class AB_3 {
     Scanner sc = new Scanner(System.in);
 
     int T = sc.nextInt();
+    int[] sum = new int[T];
 
-    for (int i = 1; i <= T; i++) {
+    for (int i = 0; i < sum.length; i++) {
       int A = sc.nextInt();
       int B = sc.nextInt();
 
-      System.out.println(A + B);
+      sum[i] = A + B;
+    }
+
+    for (int i = 0; i < sum.length; i++) {
+      System.out.println(sum[i]);
     }
   }
 }
