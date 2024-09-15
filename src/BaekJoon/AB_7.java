286c008eac48fdebcc09e0f9147e57777d78b5f0
Pjangwon <pjw051016@gmail.com>
2024-09-15 16:59:09 +0900

    11021 문제 해결 Scanner

---
+++ src/BaekJoon/AB_7.java
@@ -0,0 +1,22 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class AB_7 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int T = sc.nextInt();
+    int[] sum = new int[T];
+
+    for (int i = 0; i < sum.length; i++) {
+      int A = sc.nextInt();
+      int B = sc.nextInt();
+
+      sum[i] = A + B;
+    }
+    for (int i = 1; i <= sum.length; i++) {
+      System.out.println("Case #"+i+":" + " " + sum[i-1]);
+    }
+  }
+}
