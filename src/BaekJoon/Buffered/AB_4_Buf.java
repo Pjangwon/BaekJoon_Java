d46d60855520b004f9515a2407416f03d8e49339
Pjangwon <pjw051016@gmail.com>
2024-09-17 16:11:33 +0900

    10951 문제 해결

--- src/BaekJoon/Buffered/AB_4_Buf.java
+++ src/BaekJoon/Buffered/AB_4_Buf.java
@@ -8,20 +8,14 @@ public class AB_4_Buf {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    int N = Integer.parseInt(br.readLine());
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    int V = Integer.parseInt(br.readLine());
+    String s;
+    while ((s = br.readLine()) != null) {
+      StringTokenizer st = new StringTokenizer(s, " ");
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
 
-    int[] numStore = new int[N];
-    int count = 0;
-
-    for (int i = 0; i < N; i++) {
-      numStore[i] = Integer.parseInt(st.nextToken());
-      if (numStore[i] == V) {
-        count++;
-      }
+      bw.write((a + b) + "\n");
     }
-    bw.write(count + "\n");
     bw.flush();
   }
 }
