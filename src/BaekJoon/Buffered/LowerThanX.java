13aa9ef4bc124ff1d8af296312a1eb8c3ff6aba3
Pjangwon <pjw051016@gmail.com>
2024-09-17 23:19:12 +0900

    2024-09-17 문제 해결 완료

---
+++ src/BaekJoon/Buffered/LowerThanX.java
@@ -0,0 +1,28 @@
+package BaekJoon.Buffered;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class LowerThanX {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    StringTokenizer NandX = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(NandX.nextToken());
+    int X = Integer.parseInt(NandX.nextToken());
+
+    StringTokenizer numLineSt = new StringTokenizer(br.readLine());
+    int[] numStore = new int[N];
+
+    for (int i = 0; i < N; i++) {
+      numStore[i] = Integer.parseInt(numLineSt.nextToken());
+    }
+
+    for (int i = 0; i < N; i++) {
+      if (numStore[i] < X) {
+        bw.write(numStore[i] + " ");
+      }
+    }
+    bw.flush();
+  }
+}
---
+++ src/BaekJoon/Buffered/MaxVal.java
@@ -0,0 +1,29 @@
+package BaekJoon.Buffered;
+
+import java.io.*;
+
+public class MaxVal {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int[] arr = new int[9];
+
+    for (int i = 0; i < 9; i++) {
+      arr[i] = Integer.parseInt(br.readLine());
+    }
+
+    int max = 0;
+    int maxI = 0;
+
+    for (int i = 0; i < 9; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+        maxI = i;
+      }
+    }
+    bw.write(max + "\n");
+    bw.write((maxI+1) + "\n");
+    bw.flush();
+  }
+}
