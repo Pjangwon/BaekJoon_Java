2a565cdf9c84cafbe0991eb439c9c30e2fb2146e
Pjangwon <pjw051016@gmail.com>
2024-09-19 22:14:58 +0900

    3352 나머지 X 해결

---
+++ src/BaekJoon/Buffered/BOJ_3352_B_X.java
@@ -0,0 +1,23 @@
+package BaekJoon.Buffered;
+
+import java.io.*;
+
+public class BOJ_3352_B_X {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    boolean[] arr = new boolean[42];
+    int count = 0;
+    for (int i = 1; i <= 10; i++) {
+      arr[Integer.parseInt(br.readLine()) % 42] = true;
+    }
+    for (int i = 0; i <= 41; i++) {
+      if(arr[i]) {
+        count++;
+      }
+    }
+    bw.write(count + "\n");
+    bw.flush();
+  }
+}
