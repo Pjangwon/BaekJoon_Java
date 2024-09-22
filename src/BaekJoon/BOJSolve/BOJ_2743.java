bc3b8b7e7f96fe4b9ce49b9d6c905b620685c69b
Pjangwon <pjw051016@gmail.com>
2024-09-22 21:24:12 +0900

    BOJ 2743 단어 길이 재기 해결

---
+++ src/BaekJoon/BOJSolve/BOJ_2743.java
@@ -0,0 +1,16 @@
+package BaekJoon.BOJSolve;
+
+import java.io.*;
+
+public class BOJ_2743 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    String st = br.readLine();
+
+    bw.write(st.length() + "\n");
+    bw.flush();
+  }
+}
