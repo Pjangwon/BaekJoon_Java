83228e71cc3a527376fadb8d3fc9c03f78b67c16
Pjangwon <pjw051016@gmail.com>
2024-09-22 22:06:43 +0900

    BOJ 11654 아스키 코드 해결

---
+++ src/BaekJoon/BOJSolve/BOJ_11654.java
@@ -0,0 +1,16 @@
+package BaekJoon.BOJSolve;
+
+import java.io.*;
+
+public class BOJ_11654 {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    char ch = (char) br.read();
+    bw.write( (int) ch + "\n");
+    bw.flush();
+  }
+}
