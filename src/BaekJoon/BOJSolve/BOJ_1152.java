2856c5bfc81123d98b4ce49c654562754d1089aa
Pjangwon <pjw051016@gmail.com>
2024-09-24 10:13:18 +0900

    BOJ 1152 단어수 세기 해결

---
+++ src/BaekJoon/BOJSolve/BOJ_1152.java
@@ -0,0 +1,16 @@
+package BaekJoon.BOJSolve;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_1152 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    System.out.println(st.countTokens());
+  }
+}
