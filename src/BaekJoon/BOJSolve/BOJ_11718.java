4070b38a9e14dd9c4f42734d0e81ba4f0e2c6e4b
Pjangwon <pjw051016@gmail.com>
2024-09-26 10:08:45 +0900

    BOJ_11718 그대로 출력하기 해결

---
+++ src/BaekJoon/BOJSolve/BOJ_11718.java
@@ -0,0 +1,19 @@
+package BaekJoon.BOJSolve;
+
+import java.io.*;
+import java.lang.*;
+
+public class BOJ_11718 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    String st;
+
+    while ((st = br.readLine()) != null) {
+      bw.write(st + "\n");
+    }
+    bw.flush();
+  }
+}
