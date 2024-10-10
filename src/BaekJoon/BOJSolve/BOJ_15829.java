3b76c7f0c3944af5199552178885b7fadb9d202e
Pjangwon <pjw051016@gmail.com>
2024-10-10 15:32:37 +0900

    BOJ_15829 hashing 알고리즘 100점

--- src/BaekJoon/BOJSolve/BOJ_15829.java
+++ src/BaekJoon/BOJSolve/BOJ_15829.java
@@ -10,15 +10,18 @@ public class BOJ_15829 {
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     int N = Integer.parseInt(br.readLine());
+    int M = 1234567891;
 
     char[] qeury = br.readLine().toCharArray();
 
     long hashing = 0;
+    long power = 1;
 
     for (int i = 0; i < N; i++) {
-      hashing += ((int) qeury[i] - 96) * Math.pow(31, i);
+      hashing += (qeury[i] - 'a' + 1) * power;
+      power = (power * 31) % M;
     }
-    bw.write(hashing + "\n");
+    bw.write(hashing % M + "\n");
     bw.flush();
   }
 }
